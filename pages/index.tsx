db6433b88b146f74b32e654ba61d08fea9e32c2b
agumy <nov13sh@gmail.com>
2021-05-07 17:32:23 +0900

    Use Trending Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,35 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
+import { Trending } from "../components/Trending";
+
+type Trendings = {
+  isLoading: boolean;
+  data: any[] | null;
+  error: Error | null;
+};
 
 export default function Home() {
+  const [state, setState] = useState<Trendings>({
+    isLoading: false,
+    data: null,
+    error: null,
+  });
+
+  useEffect(() => {
+    setState((state) => ({ ...state, isLoading: true }));
+
+    (async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/trending");
+        const data = await res.json();
+        setState((state) => ({ ...state, isLoading: false, data }));
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
   return (
     <>
       <Head>
@@ -9,7 +37,15 @@ export default function Home() {
       </Head>
       <div className="h-screen w-screen flex flex-col bg-gray-900">
         <Header />
-        <main className="h-full" />
+        <main className="h-full p-5 flex justify-center">
+          <div className="w-full h-full bg-gray-700 shadow-lg rounded-lg overflow-auto">
+            <ol>
+              {state.data?.map((d) => (
+                <Trending trending={d} />
+              ))}
+            </ol>
+          </div>
+        </main>
       </div>
     </>
   );
