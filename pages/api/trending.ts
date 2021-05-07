a18a2c122a33a7304da92d7330ecd45f01d0d0cd
agumy <nov13sh@gmail.com>
2021-05-07 17:31:54 +0900

    update mock for trendings

--- pages/api/trending.ts
+++ pages/api/trending.ts
@@ -13,16 +13,20 @@ export default (req: NextApiRequest, res: NextApiResponse<Trending[]>) => {
       title: "agumy/world",
       increaseStar: 30,
       href: "https://github.com/agumy/world",
+      desc:
+        "This repository is hoge hoge hugkaa settimeout functions hello world. ",
     },
     {
       title: "agumy/playground",
       increaseStar: 20,
       href: "https://github.com/agumy/playground",
+      desc: "We should learn data structure and algorithm. ",
     },
     {
-      title: "agumy/sandbox",
+      title: "agumy/message-d",
       increaseStar: 30,
-      href: "https://github.com/agumy/sandbox",
+      href: "https://github.com/agumy/message-d",
+      desc: "I want money. ",
     },
   ];
 
